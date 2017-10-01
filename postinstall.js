9236f01f7cffaf406d5ed1b89c2b80b735910846
iamso <so@dev.so>
2017-10-01 13:45:05 +0200

    update postinstall script to match cli output

--- postinstall.js
+++ postinstall.js
@@ -1,12 +1,18 @@
 const fs = require('fs');
+const path = require('path');
+const colors = require('colors');
+const logUpdate = require('log-update');
 const request = require('./lib/request');
 
 const files = [
   'https://raw.githubusercontent.com/iamso/talkso/master/talkso.css',
   'https://raw.githubusercontent.com/iamso/talkso/master/talkso.js',
 ];
-
 const dir = `${__dirname}/client`;
+const dirName = path.basename(dir);
+
+const frames = ['—', '\\', '|', '/'];
+let i = 0;
 
 (async () => {
 
@@ -14,9 +20,17 @@ const dir = `${__dirname}/client`;
     fs.mkdirSync(dir);
   }
 
+  let logInterval = setInterval(() => {
+    const frame = frames[i = ++i % frames.length];
+    logUpdate(`${frame} updating client`.bold);
+  }, 100);
+
   for (let file of files) {
     const content = await request.get(file);
-    const fileName = file.split('/').pop();
+    const fileName = path.basename(file);
     fs.writeFileSync(`${dir}/${fileName}`, content);
   }
+  clearInterval(logInterval);
+  logUpdate(`→ client updated`.bold);
+  logUpdate.done();
 })();
